0e26ee4375b62de6b91152bce2541312e533cd21
Aline Freitas <aline.freitas@escale.com.br>
2018-09-19 17:59:11 -0300

    Fix deploy script.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-curl -X POST -H "Content-Type: application/json" --data '{"docker_tag": "'${FLAVOR}'"}' https://registry.hub.docker.com/u/escaletech/alpine-php-nginx-pagespeed/trigger/$DOCKER_HUB_TOKEN/
+STATUS=$(curl -X POST -H "Content-Type: application/json" --data '{"docker_tag": "'$1'"}' -s -o /dev/null -w "%{http_code}" https://registry.hub.docker.com/u/escaletech/alpine-php-nginx-pagespeed/trigger/$DOCKER_HUB_TOKEN/)
+if [ $STATUS -ne 200 ]; then
+    echo '{"docker_tag": "'$1'"}'
+    echo "ERROR $STATUS: Something is wrong."
+    exit 1
+fi
